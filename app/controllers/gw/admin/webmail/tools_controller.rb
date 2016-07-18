d56c6ef4c9c915d14c33adc32166802b49c02244
Yoshikazu Kaneta <kaneta@idsinc.co.jp>
2016-07-18 15:01:35 +0900

    Permit parameters for batch deletion

--- app/controllers/gw/admin/webmail/tools_controller.rb
+++ app/controllers/gw/admin/webmail/tools_controller.rb
@@ -14,7 +14,7 @@ class Gw::Admin::Webmail::ToolsController < Gw::Controller::Admin::Base
       return render :batch_delete
     end
 
-    @item = Gw::WebmailToolBatchDeleteSetting.new(params[:item])
+    @item = Gw::WebmailToolBatchDeleteSetting.new(item_params)
     unless @item.valid?
       return render :batch_delete
     end
@@ -24,5 +24,11 @@ class Gw::Admin::Webmail::ToolsController < Gw::Controller::Admin::Base
     @item.start_date = ''
     @item.end_date = ''
     flash.now[:notice] = "#{delete_num}件のメールを削除しました。"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:mailbox_id, :start_date, :end_date, :include_starred)
   end
 end
