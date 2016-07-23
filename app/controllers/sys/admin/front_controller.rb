0941c28827641b056ab872299a0ec3995fcf3cd1
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-07-23 13:30:02 +0900

    Remove unnecessary files

--- app/controllers/sys/admin/front_controller.rb
+++ app/controllers/sys/admin/front_controller.rb
@@ -2,7 +2,5 @@ class Sys::Admin::FrontController < Sys::Controller::Admin::Base
   def index
     @messages = Sys::Message.state_public.order(published_at: :desc)
     @maintenances = Sys::Maintenance.state_public.order(published_at: :desc)
-
-    #@calendar = Util::Date::Calendar.new(nil, nil)
   end
 end
