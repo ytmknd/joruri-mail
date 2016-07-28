d9bc5aaa1ca18015a3831e30ab3aff66c76c7dd5
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-07-28 17:02:23 +0900

    Fix mailbox operations associated with filters and mail nodes

--- app/controllers/gw/admin/webmail/mailboxes_controller.rb
+++ app/controllers/gw/admin/webmail/mailboxes_controller.rb
@@ -2,13 +2,14 @@ class Gw::Admin::Webmail::MailboxesController < Gw::Controller::Admin::Base
   include Sys::Controller::Scaffold::Base
   layout "admin/gw/webmail"
 
-  before_action :set_mailboxes
-  before_action :set_item, only: [:create, :update, :destroy]
   after_action :reload_mailboxes, only: [:create, :update, :destroy]
 
   def pre_dispatch
-    @mailbox  = Gw::WebmailMailbox.load_mailbox(params[:mailbox])
+    @mailboxes = Gw::WebmailMailbox.load_mailboxes
+    @mailbox = @mailboxes.detect { |box| box.name == params[:mailbox] }
     return http_error(404) unless @mailbox
+
+    @item = Gw::WebmailMailbox.load_mailboxes.detect { |box| box.name == params[:mailbox] }
   end
 
   def index
@@ -20,122 +21,116 @@ class Gw::Admin::Webmail::MailboxesController < Gw::Controller::Admin::Base
   end
 
   def create
+    return http_error(403) unless @item.creatable?
+
+    @item = Gw::WebmailMailbox.new(user_id: Core.current_user.id)
     @item.attributes = item_params
 
-    raise @item.errors.add(:base, "権限がありません。") unless @item.creatable?
-    raise unless @item.valid?
-    begin
-      Core.imap.create(@item.path + Net::IMAP.encode_utf7(@item.title))
-    rescue => e
-      raise @item.errors.add(:base, "#{e}")
+    if @item.valid?
+      begin
+        @item.create_mailbox(@item.path_and_encoded_title)
+      rescue => e
+        @item.errors.add(:base, e.to_s)
+      end
     end
 
-    flash[:notice] = '登録処理が完了しました。'
-    status = params[:_created_status] || :created
-    respond_to do |format|
-      format.html { redirect_to url_for(:action => :index) }
-      format.xml  { render :xml => item.to_xml(:dasherize => false), :status => status, :location => url_for(:action => :index) }
-    end
-  rescue => e
-    flash.now[:notice] = '登録処理に失敗しました。'
-    respond_to do |format|
-      format.html { render :action => :new }
-      format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
+    if @item.errors.present?
+      flash.now[:notice] = '登録処理に失敗しました。'
+      render :new
+    else
+      flash[:notice] = '登録処理が完了しました。'
+      redirect_to action: :index
     end
   end
 
   def update
+    return http_error(403) unless @item.editable?
+
+    old_name = @item.name
     @item.attributes = item_params
+    new_name = @item.path_and_encoded_title
 
-    raise @item.errors.add(:base, "権限がありません。") unless @item.editable?
-    raise unless @item.valid?
-    begin
-      Gw::WebmailMailNode.delete_nodes(@mailbox.name)
-      new_name = @item.path + Net::IMAP.encode_utf7(@item.title)
-      Core.imap.rename(@mailbox.name, new_name)
-
-      uids = Gw::WebmailMailNode.find_ref_nodes(@mailbox.name).map{|x| x.uid}
-      Core.imap.select('Star')
-      num = Core.imap.uid_store(uids, "+FLAGS", [:Deleted]).size rescue 0
-      Core.imap.expunge
-      if num > 0
-        Gw::WebmailMailNode.delete_ref_nodes(@mailbox.name)
-        reload_starred_mails({new_name => [:all]})
+    if @item.valid?
+      begin
+        @item.rename_mailbox(new_name)
+  
+        uids = Gw::WebmailMailNode.find_ref_nodes(old_name).map{|x| x.uid}
+        Core.imap.select('Star')
+        num = Core.imap.uid_store(uids, "+FLAGS", [:Deleted]).size rescue 0
+        Core.imap.expunge
+        if num > 0
+          Gw::WebmailMailNode.delete_ref_nodes(old_name)
+          reload_starred_mails({new_name => [:all]})
+        end
+      rescue => e
+        @item.errors.add(:base, e.to_s)
       end
-    rescue => e
-      raise @item.errors.add(:base, "#{e}")
     end
 
-    flash[:notice] = '更新処理が完了しました。'
-    respond_to do |format|
-      format.html { redirect_to gw_webmail_mailboxes_path(new_name) }
-      format.xml  { head :ok }
-    end
-  rescue => e
-    flash.now[:notice] = '更新処理に失敗しました。'
-    return respond_to do |format|
-      format.html { render :action => :index }
-      format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
+    if @item.errors.present?
+      flash.now[:notice] = '更新処理に失敗しました。'
+      render :index
+    else
+      flash[:notice] = '更新処理が完了しました。'
+      redirect_to action: :index, mailbox: new_name
     end
   end
 
   def destroy
-    raise @item.errors.add(:base, "権限がありません。") unless @item.deletable?
+    return http_error(403) unless @item.deletable?
 
+    old_name = @item.name
     delete_complete = @item.trash_box?(:children)
     short_name = @item.path.blank? ? @item.name : @item.name[@item.path.size, @item.name.size]
     new_name = "Trash.#{short_name}"
+
     if !delete_complete && Core.imap.list('', "Trash.#{short_name}")
-      raise @item.errors.add(:base, "同じ名前のフォルダーが既に存在します。")  
+      @item.errors.add(:base, '同じ名前のフォルダーが既に存在します。')
     end
 
-    begin
-      parent = @item.path.to_s.gsub(/\.+$/, '')
-      parent = "INBOX" if parent.blank?
-      if children = Core.imap.list('', "#{@item.name}.*")
-        children.each do |box|
-          Gw::WebmailMailNode.delete_nodes(box.name)
-          if delete_complete
-            Core.imap.delete(box.name)
-          #else
-          #  Core.imap.rename(box.name, "#{new_name}.#{box.name[@item.name.size + 1, box.name.size]}")
-          end
+    parent = @item.path.to_s.gsub(/\.+$/, '')
+    parent = "INBOX" if parent.blank?
 
-          uids = Gw::WebmailMailNode.find_ref_nodes(box.name).map{|x| x.uid}
-          num = Gw::WebmailMail.delete_all('Star', uids)
-          if num > 0
-            Gw::WebmailMailNode.delete_ref_nodes(box.name)
-            reload_starred_mails({new_name => [:all]}) unless delete_complete
+    if @item.errors.blank? && @item.valid?
+      begin
+        if children = Core.imap.list('', "#{@item.name}.*")
+          children.each do |box|
+            if delete_complete
+              Core.imap.delete(box.name)
+            end
+  
+            uids = Gw::WebmailMailNode.find_ref_nodes(box.name).map{|x| x.uid}
+            num = Gw::WebmailMail.delete_all('Star', uids)
+            if num > 0
+              Gw::WebmailMailNode.delete_ref_nodes(box.name)
+              reload_starred_mails({new_name => [:all]}) unless delete_complete
+            end
           end
         end
+  
+        if delete_complete
+          @item.delete_mailbox
+        else
+          @item.rename_mailbox(new_name)
+        end
+  
+        uids = Gw::WebmailMailNode.find_ref_nodes(old_name).map{|x| x.uid}
+        num = Gw::WebmailMail.delete_all('Star', uids)
+        if num > 0
+          Gw::WebmailMailNode.delete_ref_nodes(old_name)
+          reload_starred_mails({new_name => [:all]}) unless delete_complete
+        end
+      rescue => e
+        @item.errors.add(:base, e.to_s)
       end
-      Gw::WebmailMailNode.delete_nodes(@item.name)
-      if delete_complete
-        Core.imap.delete(@item.name)
-      else
-        Core.imap.rename(@item.name, new_name)
-      end
-
-      uids = Gw::WebmailMailNode.find_ref_nodes(@mailbox.name).map{|x| x.uid}
-      num = Gw::WebmailMail.delete_all('Star', uids)
-      if num > 0
-        Gw::WebmailMailNode.delete_ref_nodes(@mailbox.name)
-        reload_starred_mails({new_name => [:all]}) unless delete_complete
-      end
-    rescue => e
-      raise @item.errors.add(:base, "#{e}")
     end
 
-    flash[:notice] = '削除処理が完了しました。'
-    respond_to do |format|
-      format.html { redirect_to url_for(:action => :index, :mailbox => parent) }
-      format.xml  { head :ok }
-    end
-  rescue => e
-    flash.now[:notice] = '削除処理に失敗しました。'
-    return respond_to do |format|
-      format.html { render :action => :index }
-      format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
+    if @item.errors.present?
+      flash.now[:notice] = '削除処理に失敗しました。'
+      render :index
+    else
+      flash[:notice] = '削除処理が完了しました。'
+      redirect_to action: :index, mailbox: parent
     end
   end
 
@@ -143,14 +138,6 @@ class Gw::Admin::Webmail::MailboxesController < Gw::Controller::Admin::Base
 
   def item_params
     params.require(:item).permit(:path, :title)
-  end
-
-  def set_item
-    @item = @mailbox.dup
-  end
-
-  def set_mailboxes
-    @mailboxes = Gw::WebmailMailbox.load_mailboxes
   end
 
   def reload_mailboxes
--- app/models/gw/webmail_mailbox.rb
+++ app/models/gw/webmail_mailbox.rb
@@ -1,10 +1,7 @@
-require 'net/imap'
 class Gw::WebmailMailbox < ActiveRecord::Base
   include Sys::Model::Base
   include Sys::Model::Auth::Free
-
-  DEFAULTS = %w(Drafts Sent Archives Trash Star)
-  ORDERS = %w(INBOX Star Drafts Sent Archives virtual _etc Trash)
+  include Gw::Webmail::Mailboxes::Imap
 
   attr_accessor :path
   attr_accessor :parent, :children
@@ -13,6 +10,28 @@ class Gw::WebmailMailbox < ActiveRecord::Base
   validate :validate_title
 
   scope :readable, ->(user = Core.current_user) { where(user_id: user.id) }
+
+  after_initialize :set_defaults
+
+  with_options if: "name.present? && name_changed?" do
+    before_update :update_filters
+    before_update :update_mail_nodes
+    before_update :update_children
+  end
+
+  with_options if: "name.present?" do
+    before_destroy :disable_filters
+    before_destroy :destroy_mail_nodes
+    before_destroy :destroy_children
+  end
+
+  def filters
+    Gw::WebmailFilter.where(user_id: user_id, mailbox: name)
+  end
+
+  def mail_nodes
+    Gw::WebmailMailNode.where(user_id: user_id, mailbox: name)
+  end
 
   def creatable?
     Core.current_user.has_auth?(:manager) || user_id == Core.current_user.id
@@ -70,6 +89,10 @@ class Gw::WebmailMailbox < ActiveRecord::Base
     return @path if @path
     return "" if name !~ /\./
     name.gsub(/(.*\.).*/, '\\1')
+  end
+
+  def path_and_encoded_title
+    path + Net::IMAP.encode_utf7(title)
   end
 
   def slashed_title(char = "　 ")
@@ -155,217 +178,50 @@ class Gw::WebmailMailbox < ActiveRecord::Base
 
   private
 
+  def set_defaults
+    self.children = []
+  end
+
   def validate_title
     if title =~ /[\.\/\#\\]/
       errors.add :title, "に半角記号（ . / # \\ ）は使用できません。"
     end
   end
 
+  def rename_path(name, old_name, new_name)
+    name.gsub(/^#{Regexp.escape(old_name)}(\.|$)/, "#{new_name}\\1")
+  end
+
+  def update_filters
+    Gw::WebmailFilter.where(user_id: user_id, mailbox: name_was).each do |filter|
+      filter.update_columns(mailbox: rename_path(filter.mailbox, name_was, name))
+    end
+  end
+
+  def update_mail_nodes
+    Gw::WebmailMailNode.where(user_id: user_id, mailbox: name_was).update_all(mailbox: name)
+  end
+
+  def update_children
+    children.each do |child|
+      child.name = rename_path(child.name, name_was, name)
+      child.save
+    end
+  end
+
+  def disable_filters
+    filters.update_all(state: 'disabled', mailbox: '')
+  end
+
+  def destroy_mail_nodes
+    mail_nodes.delete_all
+  end
+
+  def destroy_children
+    children.each(&:destroy)
+  end
+
   class << self
-    def imap
-      Core.imap
-    end
-
-    def name_to_title(name)
-      name = Net::IMAP.decode_utf7(name)
-      I18n.t('webmail.mailbox.titles').each do |key, title|
-        name = name.gsub(/^#{key}(\.|$)/, title + '\1')
-      end
-      name
-    end
-
-    def imap_sorted_list_and_status(status_mailboxes = [:all], returns = ['MESSAGES', 'UNSEEN', 'RECENT'])
-      boxes, statuses = imap_list_status(status_mailboxes, returns)
-
-      need_boxes = DEFAULTS - boxes.map(&:name)
-      if need_boxes.size > 0
-        need_boxes.each { |box| imap.create(box) }
-        imap_sorted_list_and_status(status_mailboxes, returns)
-      end
-
-      return sort_list(boxes), statuses
-    end
-
-    def imap_list_status(status_mailboxes = [:all], returns = ['MESSAGES', 'UNSEEN', 'RECENT'])
-      nonexistent_or_noselect = ->(attr) {
-        attr.in?([:Nonexistent, :Noselect])
-      }
-
-      if imap.capabilities.include?('LIST-STATUS')
-        boxes, statuses = imap.list_status('', '*', returns)
-        boxes.reject! { |box| box.attr.any?(&nonexistent_or_noselect) }
-        statuses.reject! {|status| !status_mailboxes.include?(status.mailbox) } unless status_mailboxes.include?(:all)
-      else
-        boxes = imap.list('', '*')
-        boxes.reject! { |box| box.attr.any?(&nonexistent_or_noselect) }
-        statuses = []
-        boxes.each do |box|
-          if status_mailboxes.include?(:all) || status_mailboxes.include?(box.name)
-            status = imap.status(box.name, returns)
-            statuses << Net::IMAP::StatusData.new(box.name, status)
-          end
-        end
-      end
-      return boxes, statuses
-    end
-
-    def sort_list(boxes)
-      boxes = boxes.sort { |a, b| name_to_title(a.name).downcase <=> name_to_title(b.name).downcase }
-      boxes = boxes.group_by do |box|
-        name = box.name.split('.').first
-        name.in?(ORDERS) ? name : '_etc'
-      end
-      ORDERS.map { |name| boxes[name] }.compact.flatten
-    end
-
-    def load_mailbox(mailbox)
-      unless box = self.find_by(user_id: Core.current_user.id, name: mailbox)
-        sync_mailboxes([mailbox])
-        box = self.find_by(user_id: Core.current_user.id, name: mailbox)
-      end
-      box
-    end
-
-    def load_mailboxes(reloads = nil)
-      reloads = Array(reloads)
-      boxes = self.where(user_id: Core.current_user.id).order(:sort_no)
-      reloads << :all if boxes.size < DEFAULTS.size
-
-      if reloads.present?
-        Util::Database.lock_by_name(Core.current_user.account) do
-          sync_mailboxes(reloads)
-        end
-        boxes = self.where(user_id: Core.current_user.id).order(:sort_no)
-      end
-
-      make_tree(boxes)
-    end
-
-    def sync_mailboxes(reloads = [:all])
-      boxes = self.where(user_id: Core.current_user.id).order(:sort_no)
-      list_boxes, statuses = imap_sorted_list_and_status(reloads)
-
-      status_by_name = statuses.index_by(&:mailbox)
-      box_by_name = boxes.index_by(&:name)
-
-      deleted_box_names = boxes.map(&:name) - list_boxes.map(&:name)
-      deleted_box_names.each { |name| box_by_name[name].destroy if box_by_name[name] }
-
-      list_boxes.each_with_index do |list_box, idx|
-        if status = status_by_name[list_box.name]
-          box = box_by_name[list_box.name] || self.new
-          box.attributes = {
-            user_id:  Core.current_user.id,
-            sort_no:  idx + 1,
-            name:     list_box.name,
-            title:    name_to_title(list_box.name).split('.').last,
-            messages: status.attr['MESSAGES'],
-            unseen:   status.attr['UNSEEN'],
-            recent:   status.attr['RECENT']
-          }
-          box.save(validate: false) if box.changed?
-        end
-      end
-    end
-
-    def load_starred_mails(mailbox_uids = nil)
-      return if mailbox_uids == nil
-
-      imap.select('Star')
-      unstarred_uids = imap.uid_search(['UNDELETED', 'UNFLAGGED'])
-      if unstarred_uids.present?
-        num = imap.uid_store(unstarred_uids, '+FLAGS', [:Deleted]).to_a.size
-        if num > 0
-          imap.expunge
-          Gw::WebmailMailNode.delete_nodes('Star', unstarred_uids)
-        end
-      end
-
-      mailbox_uids.each do |mailbox, uids|
-        next if mailbox =~ /^(Star)$/
-
-        current_starred_uids = Gw::WebmailMailNode.find_ref_nodes(mailbox).map{|x| x.ref_uid}
-
-        imap.select(mailbox)
-        if uids.empty? || uids.include?('all') ||  uids.include?(:all)
-          new_starred_uids = imap.uid_search(['UNDELETED', 'FLAGGED'])
-        else
-          new_starred_uids = imap.uid_search(['UID', uids, 'UNDELETED', 'FLAGGED'])
-        end
-        new_starred_uids = new_starred_uids.select{|x| !current_starred_uids.include?(x) }
-        next if new_starred_uids.blank?
-
-        imap.select('Star')
-        next_uid = imap.status('Star', ['UIDNEXT'])['UIDNEXT']
-
-        imap.select(mailbox)
-        res = imap.uid_copy(new_starred_uids, 'Star')
-        next if res.name != 'OK'
-
-        # create cache
-        items = Gw::WebmailMail.fetch((next_uid...next_uid+new_starred_uids.size).to_a, 'Star')
-        items.each_with_index do |item, i|
-          if node = item.node
-            node.ref_mailbox = mailbox
-            node.ref_uid = new_starred_uids[i]
-            node.save
-          end
-        end
-      end
-    end
-
-    def load_quota(reload = false)
-      st = Gw::WebmailSetting.where(user_id: Core.current_user.id, name: 'quota_info').first_or_initialize
-
-      if !reload && st.persisted?
-        quota = Hash.from_xml(st.value) || {}
-        return quota.deep_symbolize_keys[:item]
-      end
-
-      if quota = get_quota_info
-        st.value = quota.to_xml(dasherize: false, skip_types: true, root: 'item')
-        st.save(validate: false)
-      end
-      quota
-    end
-
-    def get_quota_info
-      return unless imap.capabilities.include?('QUOTA')
-
-      res = imap.getquotaroot('INBOX')[1]
-      return unless res
-
-      usage_bytes = res.usage.to_i*1024
-      quota_bytes = res.quota.to_i*1024
-      warn_bytes = quota_bytes * Joruri.config.application['webmail.mailbox_quota_alert_rate'].to_f
-      
-      Hash.new.tap do |quota|
-        number_to_human_size = ApplicationController.helpers.method(:number_to_human_size)
-        size_options = { precision: 0, locale: :en }
-        quota[:total_bytes] = quota_bytes
-        quota[:total]       = number_to_human_size.call(quota_bytes, size_options)
-        quota[:used_bytes]  = usage_bytes
-        quota[:used]        = number_to_human_size.call(usage_bytes, size_options)
-        quota[:usage_rate]  = sprintf('%.1f', usage_bytes.to_f / quota_bytes.to_f * 100).to_f
-        quota[:usable]      = number_to_human_size.call(quota_bytes - usage_bytes, size_options) if usage_bytes > warn_bytes 
-      end
-    end
-
-    def make_tree(mailboxes)
-      mailboxes.each do |box|
-        box.children = []
-        box.parent = nil
-      end
-      hash = mailboxes.index_by(&:name)
-      mailboxes.each do |box|
-        if box.ancestor_name && hash[box.ancestor_name]
-          hash[box.ancestor_name].children.push(box)
-          box.parent = hash[box.ancestor_name]
-        end
-      end
-      mailboxes
-    end
-
     def mailbox_options(mailboxes = self.load_mailboxes, &block)
       mailboxes.select { |box| block.nil? || block.call(box) }
         .map {|box| [box.slashed_title, box.name] }
