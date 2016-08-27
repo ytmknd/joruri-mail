1ef3a13ba170d93b795ada556bb78d60679e9892
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-08-27 10:27:36 +0900

    Fix warning: Overwriting existing method Webmail::Doc.state_public

--- app/models/webmail/doc.rb
+++ app/models/webmail/doc.rb
@@ -1,6 +1,5 @@
 class Webmail::Doc < ApplicationRecord
   include Sys::Model::Base
-  include Sys::Model::Base::Page
   include Sys::Model::Auth::Manager
 
   belongs_to_active_hash :status, foreign_key: :state, class_name: 'Sys::Base::Status'
@@ -11,5 +10,9 @@ class Webmail::Doc < ApplicationRecord
 
   def readable?
     true
+  end
+
+  def states
+    [['公開','public'],['非公開','closed']]
   end
 end
