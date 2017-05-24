f0d3950bf539c11e6ec9d79e7307033561fd5930
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-05-24 11:33:27 +0900

    Change log level for errors on parsing mail source

--- lib/plugins/joruri/lib/shortcut_methods.rb
+++ lib/plugins/joruri/lib/shortcut_methods.rb
@@ -2,6 +2,10 @@ def error_log(message)
   Rails.logger.error "[ USER ERROR #{Time.now.strftime('%Y-%m-%d %H:%M:%S')} ]: #{message}"
 end
 
+def warn_log(message)
+  Rails.logger.warn "[ WARN #{Time.now.strftime('%Y-%m-%d %H:%M:%S')} ]: #{message}"
+end
+
 def debug_log(data)
   Rails.logger.debug "[ DEBUG #{Time.now.strftime('%Y-%m-%d %H:%M:%S')} ]: #{data.pretty_inspect}"
 end
